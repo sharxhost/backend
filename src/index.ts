08f51cfadcfb8d3a5f4c816383090d9b1eca7def
GGORG <GGORG0@protonmail.com>
2022-06-11 11:40:10 +0200

    Added signale logger and a basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import "dotenv/config";
+import express from 'express';
+import signale from "signale";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => signale.success(`[*] Listening on port ${port}`));
