12018f831f5a65220433c4df6efa033081e94686
GGORG <GGORG0@protonmail.com>
2022-06-11 11:43:30 +0200

    Added hello world route

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,8 @@ import signale from "signale";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => signale.success(`Listening on port ${port}`));
