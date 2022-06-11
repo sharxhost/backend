6d2d199b748cc9e44f2b710acc62be4ee7d9ff16
GGORG <GGORG0@protonmail.com>
2022-06-11 11:40:54 +0200

    🔊 Small log change

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import signale from "signale";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => signale.success(`[*] Listening on port ${port}`));
+app.listen(port, () => signale.success(`Listening on port ${port}`));
