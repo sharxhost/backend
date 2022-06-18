6755ab18fa7eb1a7a1a0de5ae7b7078857f30942
GGORG <GGORG0@protonmail.com>
2022-06-18 16:08:59 +0200

    🚨 Fixed ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
+// For running in pm2
+
 module.exports = {
-	apps: [
-		{
-			name: 'cordless_bot',
-			script: './dist/index.js',
-			watch: true,
-			env: {
-				IN_PM2: true,
-			},
-		},
-	],
+  apps: [
+    {
+      name: "sharx_backend",
+      script: "./dist/index.js",
+      watch: true,
+      env: {
+        IN_PM2: true,
+      },
+    },
+  ],
 };
